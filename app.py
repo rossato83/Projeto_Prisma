e3ef711c0d060aabafb8f71c488e2e5d27d06903
Almir Ricardo Rossato <almir.rossato@yssy.com.br>
2025-02-28 13:34:15 -0300

    Corrigindo a porta do Flask para Render

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Render define a porta automaticamente
+    app.run(host="0.0.0.0", port=port, debug=False)
+
+
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
